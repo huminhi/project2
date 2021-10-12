6b230345d872cd7aadd42816fe2bb3534da26bd8
Hieu Huynh <hh250198@ncr.com>
2021-10-12 17:11:31 +0700

    update project and report

--- analyze.py
+++ analyze.py
@@ -2,9 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-
-# % matplotlib inline
+from matplotlib.ticker import FuncFormatter
 
 
 def save_plot(plot_name):
@@ -15,29 +13,86 @@ def group_and_sum(dataframe, group_by):
     return dataframe.groupby(group_by).sum().reset_index()
 
 
+def thousands(x, pos):
+    return '%1.1fK' % (x * 1e-3)
+
+
+# format to thousands in unit
+formatter = FuncFormatter(thousands)
+
+
+def create_axis_with_format():
+    # plot permit against handgun and long_gun for each state
+    fig, ax = plt.subplots(figsize=(10, 8))
+    # apply thousand formatter
+    ax.yaxis.set_major_formatter(formatter)
+    return ax
+
+
 # pandas dataframe output configs
 desired_width = 200
 pd.set_option('display.width', desired_width)
 np.set_printoptions(linewidth=desired_width)
 pd.set_option('display.max_columns', 30)
-figsize = (10, 8)
 
 df = pd.read_csv("gun-data.csv")
+# print out dataframe info
 print(df.head())
 print(df.shape)
 
 # fill with O would make sense so we will not change value of total column
 df.fillna(0, inplace=True)
 print(df.info())
+# extract year from month column
 df['year'] = df['month'].str.extract(r'(\d+)-\d+', expand=True)
-
+# group data with state and year columns and sum other columns
 grouped_by_year_state = group_and_sum(df, ['year', 'state'])
+# group data with state column only and sum other columns
 states = group_and_sum(grouped_by_year_state, ['state']).sort_values('totals', ascending=False).head(5)['state']
+# select top 5 states with most totals of permit checks and gun registrations
 top_5_totals = grouped_by_year_state[grouped_by_year_state['state'].isin(states)]
-top_5_totals.pivot(index='year', columns='state', values='totals').plot(figsize=figsize)
+top_5_totals.pivot(index='year', columns='state', values='totals').plot(title='Total gun checks/permits; Figure 1', ax=create_axis_with_format())
+# save fig1 top 5 states with most totals of permit checks and gun registrations
 save_plot('fig1.png')
-
+# select top state that has most totals number
 kentucky = grouped_by_year_state[grouped_by_year_state['state'] == 'Kentucky'].drop('totals', axis=1)
-kentucky.plot(x='year', kind='area', stacked=True, figsize=figsize)
+# save fig2
+kentucky.plot(x='year', kind='area', stacked=True, ax=create_axis_with_format(), title='Gun check types in Kentucky; Figure 2')
 save_plot('fig2.png')
-# plt.show()
+
+# read census data
+census_data = pd.read_csv("u.s.-census-data.csv")
+# select Fact and top 5 state columns only
+census_data = census_data.loc[:, ['Fact'] + list(states)]
+# fill NaN columns with 0s
+census_data = census_data.astype(object).replace(np.nan, 0)
+# set index for dataframe
+census_data.set_index('Fact', inplace=True)
+# transpose census data
+census_data = census_data.T.copy()
+# rename columns
+census_data.rename(columns={'Population estimates, July 1, 2016,  (V2016)': 'population',
+                            'Housing units,  July 1, 2016,  (V2016)': 'housing'}, inplace=True)
+# select population and housing column only
+census_data = census_data.loc[:, ['population', 'housing']]
+# replace commas in all columns
+census_data.replace(',', '', regex=True, inplace=True)
+census_data = census_data.astype(int)
+# add year and state columns to census data
+census_data['year'] = '2016'
+census_data['state'] = census_data.index
+# select data in 2016
+top_5_2016 = top_5_totals[top_5_totals['year'] == '2016']
+# join with census data
+top_5_2016 = top_5_2016.set_index(['state', 'year']).join(census_data.set_index(['state', 'year']),
+                                                          on=['state', 'year'], how='left', rsuffix='census')
+# reset index to plot
+top_5_2016 = top_5_2016.reset_index()
+# plot population and permit for each state
+top_5_2016.plot(x='state', y=['population', 'permit'], kind='bar', alpha=0.5, label=['population', 'permit'], ax=create_axis_with_format(), title='Population vs Gun Permits in 2016; Figure 3')
+save_plot("fig3.png")
+
+# plot top 5 states in terms of permit, handgun, long_gun and other
+top_5_2016.plot(x='state', y=['permit', 'handgun', 'long_gun', 'other'], kind='bar', alpha=0.5, label=['permit', 'handgun', 'long_gun', 'other'], ax=create_axis_with_format(), title='Permit vs Handgun, Long Gun, and Other Firearms in 2016; Figure 4')
+save_plot("fig4.png")
+
--- report.md
+++ report.md
@@ -6,6 +6,11 @@
 
 ### Investigation ###
 
+Since data has lots of null values, we have to fill in these cells. O-filling makes sense here because we dont want to make false sum of checks/registrations
+Then, extract year from month column and group data in year-wise will give better visualization.
+Group data in year and state to find out top 5 states (https://stackoverflow.com/questions/31569549/how-to-groupby-a-dataframe-in-pandas-and-keep-columns)
+Setting graph unit to thousands (https://stackoverflow.com/questions/53747298/how-to-format-seaborn-matplotlib-axis-tick-labels-from-number-to-thousands-or-mi)
+
 * Which states (top 5) have most gun checks/registrations over years?
 
 Below figure shows that California, Illinois, Kentucky, Pennsylvania, and Texas are the top 5 states that have the most gun checks/registrations over years.
@@ -14,14 +19,33 @@ Below figure shows that California, Illinois, Kentucky, Pennsylvania, and Texas 
 
 * What are types of gun checks/registrations done in the top state?
 
+Plot permit checks, registrations in top state to find out trend using area chart (https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.plot.area.html)
+
 From Figure 1, Kentucky seems to be top state that has highest gun check/registrations.
 Below figure shows that permit checks takes the highest portion of gun checks followed by handgun registrations in Kentucky.
 
 ![Figure 2](fig2.png "Figure 2")
 
-### Data wrangling done ###
-* Since data has lots of null values, we have to fill in these cells. O-filling makes sense here because we dont want to make false sum of checks/registrations
-* Extract year from month column and group data in year-wise will give better visualization.
-* Group data in year and state to find out top 5 states (https://stackoverflow.com/questions/31569549/how-to-groupby-a-dataframe-in-pandas-and-keep-columns)
-* Plot top 5 states (https://stackoverflow.com/questions/29233283/plotting-multiple-lines-in-different-colors-with-pandas-dataframe)
-* Plot check/registration checks in top state to find out trend using area chart (https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.plot.area.html) 
+Overall, permit checks take largest volume in Kentucky since 2005 
+
+* How is gun permits vs population in the top 5 States in 2016?
+
+We need census data and transpose it following tips from https://knowledge.udacity.com/questions/713929 
+
+![Figure 3](fig3.png "Figure 3")
+
+Data in Kentucky in 2016 shows that gun permit number is nearly equal to population. Nearly, 1 person in Kentucky has 1 permit in 2016. Meanwhile, gun permits in other 4 states are relatively low comparing with population.
+
+_Limitation:_ if we have data of permits in city vs rural areas, we can have better view of trend of gun owning in different areas or cities.
+
+* How is gun permits vs handgun, long guns, other firearms in top 5 states in 2016?
+
+![Figure 4](fig4.png "Figure 4")
+
+Even though permits on population ratio is high in Kentucky; but handgun, long gun, and other registrations are very low. However, number of handguns and long guns seem to be twice number of permits in Texas. We can assume that at 1 person with permit in Texas can own a least 2 guns.
+
+In addition, total of handguns and long guns in California in 2016 is equal to number of permits. We can assume that 1 person in California with permit can own either a handgun or a long gun.
+
+While in Pennsylvania, 1 person with gun permit can own at least 2 handguns.
+
+_Limitation:_ We do not know the age of gun owners and their gun references.
